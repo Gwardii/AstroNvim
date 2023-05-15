914469b1a5174ada925cbb8cf353ba7fd8268bc1
AzureAD\FilipGwardecki <f.gwardecki@quickersim.com>
2023-05-15 09:22:18 +0200

    add copilot and fix js dap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,6 @@ return{
   {
     "github/copilot.vim",
     event = "InsertEnter",
-    config = function() vim.cmd "source ~/.config/nvim/extra_conf.vim" end,
+    config = function() vim.cmd "source ~/AppData/Local/nvim/extra_conf.vim" end,
   },
 }
