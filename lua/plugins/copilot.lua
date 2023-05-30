79b6906eb18bb2337d45df54e9d2ad357229665b
AzureAD\FilipGwardecki <f.gwardecki@quickersim.com>
2023-05-30 12:12:19 +0200

    fix copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,49 @@
 return{
   {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
     event = "InsertEnter",
-    config = function() vim.cmd "source ~/AppData/Local/nvim/extra_conf.vim" end,
+    opts = {
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>"
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-a>",
+          accept_word = "<M-w>",
+          accept_line = "<M-j>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = 'node', -- Node.js version must be > 16.x
+      server_opts_overrides = {},
+    },
   },
 }
